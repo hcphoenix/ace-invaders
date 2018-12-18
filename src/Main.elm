8caa7a0f85919643a5b1a6ca76917e4fc060e6ee
Haley Cadence Phoenix <cadence@Haleys-MacBook-Pro.local>
2018-12-17 18:04:14 -0700

    Draw a black box

--- src/Main.elm
+++ src/Main.elm
@@ -1,1 +1,46 @@
---Hello world :3
+import Browser
+import Html exposing (Html, text)
+
+import Game.TwoD as Game
+import Game.TwoD.Camera as Camera
+import Game.TwoD.Render as Render
+import Color exposing (Color)
+
+main : Program () Model Msg
+main = Browser.element
+    { init = always init
+    , view = view
+    , update = update
+    , subscriptions = subs
+    }
+
+type alias Model =
+    { time : Float
+    }
+
+init : (Model, Cmd Msg)
+init = ({time = 0}, Cmd.none)
+
+type Msg = NoOp
+
+update : Msg -> Model -> (Model, Cmd Msg)
+update msg model = (model, Cmd.none)
+
+subs : Model -> Sub Msg
+subs model = Sub.none
+
+view : Model -> Html Msg
+view model = Game.renderCentered
+    { time = model.time
+    , size = (800,600)
+    , camera = Camera.fixedWidth 10 (800,600)
+    }
+    [ background
+    ]
+
+background = Render.shape
+    Render.rectangle
+        { color = Color.black
+        , position = (0,0)
+        , size = (800,600)
+        }
